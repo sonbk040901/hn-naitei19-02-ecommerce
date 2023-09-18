c1dc78f577e4762783ac0ccb579abfc779d10f58
nguyenhoanganhNH1 <anhnh1@hblab.vn>
2023-09-18 10:27:08 +0700

    Design static page

---
+++ src/main/java/com/ecommerce/controller/PageController.java
@@ -0,0 +1,18 @@
+package com.ecommerce.controller;
+
+import java.util.List;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+//@RestController
+@Controller
+public class PageController {
+
+	@RequestMapping("/")
+	public String homepage() {
+		return "index";
+	}
+}
--- src/test/java/com/ecommerce/EcommerceApplicationTests.java
+++ src/test/java/com/ecommerce/EcommerceApplicationTests.java
@@ -3,7 +3,7 @@ package com.ecommerce;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes= EcommerceApplicationTests.class)
 class EcommerceApplicationTests {
 
 	@Test
